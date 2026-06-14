816b4eb49e879378def878c5d53a278b9f8e2044
clementsze <58460868-clementsze@users.noreply.replit.com>
2026-06-14 08:40:55 +0000

    Git commit prior to merge

--- artifacts/mobile/metro.config.js
+++ artifacts/mobile/metro.config.js
@@ -1,3 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+  ...config.resolver,
+  resolveRequest: (context, moduleName, platform) => {
+    if (moduleName.includes('virtualview/VirtualView') && moduleName.includes('NativeComponent')) {
+      return {
+        filePath: path.resolve(__dirname, 'stubs/VirtualViewNativeComponentStub.js'),
+        type: 'sourceFile',
+      };
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
+};
+
+module.exports = config;
